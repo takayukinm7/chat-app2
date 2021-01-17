f81b472473bc1642847b04892a9f2325148b2be0
田中貴幸 <tanakatakayuki@tanakatakayukinoMacBook-Pro.local>
2021-01-17 15:55:04 +0900

    各モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :room_users
+  has_many :room_user
   has_many :rooms, through: :room_users
   has_many :messages
+
+  validates :name, presence:true
 end
